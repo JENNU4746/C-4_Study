01fdd58af52ee389a81132f09252af9de6d67899
JENNU4746 <jennu4746@icloud.com>
2017-07-19 15:27:18 +0900

    Studyed CS Variable

--- Program.cs
+++ Program.cs
@@ -1,15 +1,33 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace C_4_Study
 {
+    class CSVar
+    {
+        //필드(전역변수)
+        int globalV;
+        const int MAX = 1024;
+
+        public void Method()
+        {
+            //로컬 변수
+            int localV = MAX;//로컬 변수는 할당 필수
+
+            PrintC(localV);
+            PrintC(globalV);
+        }
+
+        private void PrintC(int num)
+        {
+            Console.WriteLine(num);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            CSVar obj = new CSVar();
+            obj.Method();
         }
     }
 }
